cb1bcc15b8a9bebdb4be1f4577cd7f55fb881fb5
Asjas <asjas@outlook.com>
2019-07-30 22:29:48 +0200

    Add Filtering, Pagination and Sorting to the GraphQL backend

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -1,6 +1,24 @@
 const Query = {
   async feed(parent, args, ctx, info) {
-    return await ctx.prisma.links();
+    const where = args.filter
+      ? {
+          OR: [{ description_contains: args.filter }, { url_contains: args.filter }],
+        }
+      : {};
+
+    const links = await ctx.prisma.links({
+      where,
+      skip: args.skip,
+      first: args.first,
+      orderBy: args.orderBy,
+    });
+
+    const count = await ctx.prisma
+      .linksConnection({ where })
+      .aggregate()
+      .count();
+
+    return { links, count };
   },
 };
 
